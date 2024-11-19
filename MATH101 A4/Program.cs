56153eba7d276d75c2a9f78595638adf3da53b96
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-11-19 18:08:25 +1030

    Add project files.

---
+++ MATH101 A4/Program.cs
@@ -0,0 +1,140 @@
+﻿
+namespace MATH101_A4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            question1();
+            question2();
+            question3();
+            question4();
+            question5();
+        }
+
+
+
+        static void question1()
+        {
+            Random random = new Random();
+
+            float X1 = random.Next(0,200);
+            float Y1 = random.Next(0,200);
+
+            float X2 = random.Next(0,200);
+            float Y2 = random.Next(0,200);
+
+            Vec vec1 = new Vec(X1, Y1);
+            Vec vec2 = new Vec(X2, Y2);            
+
+            //Vec vec3 = Vector2.vectorFrom2Points(vec1, vec2);
+            Vec vec3 = mathLib.vectorFrom2Points(vec1, vec2);
+            float magnitude = mathLib.Magnitude(vec3);
+
+            Console.WriteLine($"Q1 Magnitude = {magnitude}\n");
+        }
+
+        static void question2()
+        {
+            // roomba
+            Vec roombaVector = new(12.0f, 3.0f); // roomba is at 12,3
+            Obj Roomba = new(roombaVector);
+
+            //default
+            //Vec keyID0 = new(0.0f, 1.0f);  // up
+            //Vec keyID1 = new(1.0f, 0.0f);  // right
+            //Vec keyID2 = new(0.0f, -1.0f); // down
+            //Vec keyID3 = new(-1.0f, 0.0f); // left
+
+            Vec keyID0 = new(0.0f, 0.0f);  // up
+            Vec keyID1 = new(1.0f, 0.0f);  // right
+            Vec keyID2 = new(0.0f, -1.0f); // down
+            Vec keyID3 = new(0.0f, 0.0f); // left
+
+            Vec movementDirection = mathLib.addVectorsTogether(keyID0, keyID1, keyID2, keyID3);
+            Vec normalizedMovement = mathLib.Normalize(movementDirection);
+
+            Console.WriteLine($"Q2 magnitude = {mathLib.Magnitude(normalizedMovement)}");
+            Console.WriteLine($"initial position = {Roomba.positionFromOrigin.X},{Roomba.positionFromOrigin.Y}");
+
+            Vec newPosition = mathLib.addVectorsTogether(normalizedMovement,roombaVector);
+            Roomba.positionFromOrigin = newPosition;
+            Console.WriteLine($"new Movement = {normalizedMovement.X},{normalizedMovement.Y}");
+            Console.WriteLine($"new position = {Roomba.positionFromOrigin.X},{Roomba.positionFromOrigin.Y}\n");
+
+        }
+
+        static void question3()
+        {
+            
+            int intensity = 2;
+            Random random = new Random();
+
+            float randomDegree = random.Next(0,360);
+            randomDegree = -180;
+            Console.WriteLine($"Question 3 \nDegree = {randomDegree}");
+            Vec lightPulse = mathLib.genFromAngle(randomDegree, intensity);
+            Console.WriteLine($"Lightpulse = {lightPulse.X},{lightPulse.Y}\n");
+        }
+
+        static void question4()
+        {
+            /*float X1 = random.Next(0,200);
+            float Y1 = random.Next(0,200);
+            float X2 = random.Next(0,200);
+            float Y2 = random.Next(0,200);*/
+
+            float X1 = 1.0f;
+            float Y1 = 1.0f;
+
+            float X2 = 1.5f;
+            float Y2 = 1.5f;
+
+
+            Vec vec1 = new(X1, Y1);
+            Obj spaceShip = new(vec1, 4.0f);
+            Vec vec2 = new(X2, Y2);
+            Obj spaceDebris = new(vec2, 4.0f);
+
+            float intersectionValue = mathLib.Collision(spaceDebris, spaceShip);
+            Console.WriteLine(intersectionValue);
+            if (intersectionValue >= 0.0f)
+            {
+                Console.WriteLine("Objs Collided");
+            }
+            else
+            {
+                Console.WriteLine("we have safely avoided the debris");
+            }
+        }
+
+        static void question5()
+        {
+            // uses dot product
+            //initialize value
+
+            float X1 = 0.0f;
+            float Y1 = 0.0f;
+
+            float X2 = 30.0f;
+            float Y2 = 30.0f;
+            Vec vec1 = new(X1, Y1);
+            Obj Sun = new(vec1, 1.0f);
+            Vec vec2 = new(X2, Y2);
+            Obj solarPanel = new(vec2, 0.5f);
+
+            //^ ignore ^
+
+
+            //sun to space ship x and y
+            Vec sunToSpaceShip = mathLib.vectorFrom2Points(
+                Sun.positionFromOrigin, solarPanel.positionFromOrigin);
+
+            //solar panel normal (spnx, spny)
+            Vec solarePanelNormal = mathLib.genFromAngle(225.0f);
+
+            float percentage =mathLib.DotProduct(sunToSpaceShip, solarePanelNormal);
+           Console.WriteLine($"{percentage}%");
+        }
+    }
+}
---
+++ MATH101 A4/mathLib.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATH101_A4
+{
+
+    struct Vec 
+    {
+        public float X=0;
+        public float Y;
+        public Vec(float X, float Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public Vec()
+        {
+            X = 0.0f;
+            Y = 0.0f;
+        }
+
+    }
+
+    struct Obj
+    {
+        public Vec positionFromOrigin;
+        public float sizeInRadius;
+        public Obj(Vec position, float size = 0.0f)
+        {
+            positionFromOrigin = position;
+            sizeInRadius = size;
+        }
+    }
+
+
+    abstract class mathLib
+    {
+        private readonly Vec defaultVec = new Vec(0.0f,0.0f);
+        internal static Vec scaleVector(Vec vector, float scale)
+        {
+            float X = vector.X * scale;
+            float Y = vector.Y * scale;
+
+            // create vector and return
+            Vec returningVector = new(X, Y);
+            return returningVector;
+        }
+
+
+        internal static Vec vectorFrom2Points(Vec point1,Vec point2)
+        {
+            float X = point2.X - point1.X;
+            float Y = point2.Y - point1.Y;
+            Vec newVector = new (X, Y);
+            return newVector;
+        }
+
+        internal static Vec addVectorsTogether(Vec v1 , Vec v2, Vec v3, Vec v4)
+        {
+            float X = v1.X + v2.X + v3.X + v4.X;
+            float Y = v1.Y + v2.Y + v3.Y + v4.Y;
+            Vec returningVector = new(X, Y);
+            return returningVector;
+        }
+        internal static Vec addVectorsTogether(Vec v1, Vec v2)
+        {            
+            return addVectorsTogether(v1, v2, new Vec(), new Vec());
+        }
+        internal static Vec addVectorsTogether(Vec v1, Vec v2, Vec v3)
+        {
+            return addVectorsTogether(v1, v2, v3, new Vec());
+        }
+
+
+
+
+        internal static float Magnitude(Vec vector)
+        {
+            float Magnitude = MathF.Sqrt((vector.X * vector.X) + (vector.Y * vector.Y));
+            return Magnitude;
+        }
+
+        internal static Vec Normalize(Vec vector)
+        {
+            float Magnitude = mathLib.Magnitude(vector);
+            float normalizedX = vector.X / Magnitude;
+            float normalizedY = vector.Y / Magnitude;
+
+            Vec normalized = new Vec(normalizedX, normalizedY);
+
+            return normalized;
+        }
+
+        internal static Vec genFromAngle(float Degrees, float magnitude = 5.0f)
+        {
+            // degrees to radians
+            float Radians = Degrees * (3.14159265f) / 180;
+            float X = magnitude * MathF.Cos(Radians);
+            float Y = magnitude * MathF.Sin(Radians);
+
+            // create vector and return
+            Vec returningVector = new Vec(X, Y);
+
+            return returningVector;
+        }
+
+
+
+        internal static float Collision(Obj object1, Obj object2)
+        {
+            Vec vectorBetween2Points = vectorFrom2Points(object1.positionFromOrigin, object2.positionFromOrigin);
+            float distance = Magnitude(vectorBetween2Points);
+
+            float distanceBeforeCollision = object1.sizeInRadius + object2.sizeInRadius;
+
+            float collision = distanceBeforeCollision - distance;
+            // float collision = distance;
+            // float collision = distanceBeforeCollision;
+
+            return collision;
+        }
+
+        internal static float DotProduct(Vec vector1, Vec vector2)
+        {
+
+            // normalize our vector
+            Vec normalizedVector1 = Normalize(vector1);
+            Vec normalizedVector2 = Normalize(vector2);
+
+            // dot product
+            float percentage = normalizedVector1.X * -normalizedVector2.X +
+                               normalizedVector1.Y * -normalizedVector2.Y;
+
+            // gives value 0-1
+
+            // this is the step that i missed
+            percentage *= 100.0f;
+            return percentage;
+        }
+    }
+}
